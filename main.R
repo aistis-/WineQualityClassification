ce5dc88af698f1a7ab7739bb3efeceec52210f65
Aistis <aistis8624@gmail.com>
2015-01-15 16:29:42 +0100

    Add whie wine data set choice

--- main.R
+++ main.R
@@ -4,32 +4,41 @@ println <- function(...) {
 }
 
 # Read red wines data set to a table
-redWinesFull <- read.table(file="data_set/red_wines.csv", header=TRUE, quote="\"", sep=";")
+redWines <- read.csv(file="data_set/red_wines.csv", sep=";")
+whiteWines <- read.csv(file="data_set/red_wines.csv", sep=";")
+
+# Data set for the script. Red or white wines
+data = redWines
+#data = whiteWines
 
 # 1-11 attributes are the main values for classification
-redWinesValues = redWinesFull[,1:11]
+dataValues = data[,1:11]
 
 # The 12th attribute is our targeted quality value
-redWinesTarget = redWinesFull[,12]
+dataTarget = data[,12]
 
-println("Number of attributes: ", ncol(redWinesValues))
+println("Number of attributes: ", ncol(dataValues))
 
 # Check if there are any missing valuse
-if (0 == sum(is.na(redWinesValues)))
+if (0 == sum(is.na(dataValues)))
   println("There is no missing values")
 else 
-  println("Missing values:", sum(is.na(redWinesValues)))
+  println("Missing values:", sum(is.na(dataValues)))
 
 # Print attributes summary
 println("Data summary: ")
 
-for(i in 1:ncol(redWinesValues)) {
-  println("Attribute:", names(redWinesValues[i]))
-  println("  Mode:", mode(redWinesValues[,i]))
-  println("  Mean:", mean(redWinesValues[,i]))
-  println("  Median:", median(redWinesValues[,i]))
-  println("  Min:", min(redWinesValues[,i]))
-  printlnt("  Max:", max(redWinesValues[,i]))
-  println("  Max - min:", max(redWinesValues[,i]) - min(redWinesValues[,i]))
+for(i in 1:ncol(dataValues)) {
+  println("Attribute:", names(dataValues[i]))
+  println("  Mode:", mode(dataValues[,i]))
+  println("  Mean:", mean(dataValues[,i]))
+  println("  Median:", median(dataValues[,i]))
+  println("  Min:", min(dataValues[,i]))
+  println("  Max:", max(dataValues[,i]))
+  println("  Max - min:", max(dataValues[,i]) - min(dataValues[,i]))
+  println("  Standard deviation:", sd(dataValues[,i]))
   println();
 }
+
+boxplot(dataValues[,1:11])
+plot(dataValues[,1:11])
